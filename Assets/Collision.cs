a1336ccf8b5eba5f42ed894d91e9335cfb0162a0
DESKTOP-N5R9FT2\Fritz <fritzjohn_solibio@yahoo.ca>
2023-02-19 23:51:12 -0800

    added sprite assets

---
+++ Assets/Collision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hitt!!!!!!!!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("was HIT");
+    }
+}
