8370d4a2bf1e4fee98e854adef3a1a4895843d3e
DESKTOP-N5R9FT2\Fritz <fritzjohn_solibio@yahoo.ca>
2023-02-20 16:29:39 -0800

    Project complete

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColour = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColour = new Color32(1, 1, 1, 1);
+    [SerializeField] float deleteDelay = 0.5f;
+    bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("hitt!!!!!!!!");
@@ -9,6 +21,17 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("was HIT");
+        if (collision.tag.Equals("Package") && !hasPackage)
+        {
+            Destroy(collision.gameObject, deleteDelay);
+            spriteRenderer.color = hasPackageColour;
+            hasPackage = true;
+        }
+
+        if (collision.tag.Equals("Customer") && hasPackage)
+        {
+            spriteRenderer.color = noPackageColour;
+            hasPackage = false;
+        }
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,10 +4,13 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float slowSpeed = 1f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -18,5 +21,19 @@ public class Driver : MonoBehaviour
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("SpeedUp"))
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+
+    private void onCollisionEnter2D(Collider2D collision)
+    {
+        moveSpeed = slowSpeed;
     }
 }
